33ee11f6ec7036d2f4230a0dc5bd676b6db653e1
Lambert-Charles <charles-lambert@outlook.com>
2024-03-30 18:20:40 +0100

    changing compiler ressource to Java 1.8

--- src/main/java/mavenTest/Application.java
+++ src/main/java/mavenTest/Application.java
@@ -1,7 +1,19 @@
 package mavenTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for(String s: greetings){
+			System.out.println("Greeting: " + s);
+		}
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -9,6 +21,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		app.greet();
     }
 }
